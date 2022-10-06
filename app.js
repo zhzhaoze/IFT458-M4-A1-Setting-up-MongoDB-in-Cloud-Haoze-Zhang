054538f39d99a6a0c4a295d67e26dd7d8aedd4b6
Haoze Zhang <429677043@qq.com>
2022-10-05 22:21:21 -0700

    IFT458-M4-A1

---
+++ app.js
@@ -0,0 +1,34 @@
+//** Code START
+// 3) ROUTES
+const express = require('express');
+const morgan = require('morgan');
+
+const courseRouter = require('./routes/courseRoutes');
+const userRouter = require('./routes/userRoutes');
+
+const app = express();
+
+// 1) MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware 👋');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// 3) ROUTES
+app.use('/api/v1/courses', courseRouter);
+//app.use('/api/v1/users', userRouter);
+
+module.exports = app;
+//** code END
---
+++ schemas/loanSchema.js
@@ -0,0 +1,29 @@
+//Create schema
+// this.rawData404 = new JSONSchemaView(this.obj);
+const mongoose = require('mongoose')
+
+const { Schema } = mongoose;
+
+const customerSchema = new Schema({
+  // String is shorthand for {type: String}
+  customerName:   String,
+  phoneNumber:   String,
+  address:   String,
+  loanAmount:   Number,
+  interest:   Number,
+  loanTermYears: Number,
+  loanType: String,
+  description:  String,
+  // createdDate: {type: Date, default: Date.now},
+  // insertedDate: {type: Date, default: Date.now}
+
+});
+
+//Create the Schema Definition
+const Customer = mongoose.model('Customers', customerSchema);
+// module.exports = Customer
+
+console.log(Customer);
+console.log(customerSchema)
+
+
